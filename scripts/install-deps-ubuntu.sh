5faf084623cfd4fbb3dfdc66d5232c4c7b21914a
KM <the.real.laplace@gmail.com>
2022-11-04 02:16:04 +0200

    scripts

---
+++ scripts/install-deps-ubuntu.sh
@@ -0,0 +1,11 @@
+
+sudo apt update
+sudo apt install \
+  build-essential \
+  libgl1-mesa-dri \
+  libglapi-mesa \
+  libglew-dev \
+  libglu1-mesa-dev \
+  libosmesa6 \
+  libxi-dev mesa-utils \
+  pkg-config
