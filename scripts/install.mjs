36eba131cedf9b08f93f047695e6d9380db64910
KM <the.real.laplace@gmail.com>
2024-05-06 17:23:02 +0100

    types

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -2,8 +2,10 @@
 if (!process.env.GL_FROM_SOURCE) {
 	try {
 		await import('./download-release.mjs')
-		process.exit()
-	} catch (error) {}
+		process.exit(0)
+	} catch (error) {
+		console.log("failed to download release")
+	}
 } else {
 	console.log("skip download and build from source")
 }
